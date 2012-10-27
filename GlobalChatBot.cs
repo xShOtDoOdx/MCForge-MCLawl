a7872afd9a4435d5262f2897464dae64a274bbd6
501st_commander <luaeditor94@gmail.com>
2012-10-26 20:26:01 -0500

    Fixed GC whitespace issue

--- GlobalChatBot.cs
+++ GlobalChatBot.cs
@@ -24,8 +24,10 @@ using System.Text.RegularExpressions;
 using Sharkbite.Irc;
 //using System.Threading;
 
-namespace MCForge {
-    public class GlobalChatBot {
+namespace MCForge
+{
+    public class GlobalChatBot
+    {
         public delegate void RecieveChat(string nick, string message);
         public static event RecieveChat OnNewRecieveGlobalMessage;
 
@@ -42,16 +44,21 @@ namespace MCForge {
 
         const string caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
         const string nocaps = "abcdefghijklmnopqrstuvwxyz ";
-        public GlobalChatBot(string nick) {
+        public GlobalChatBot(string nick)
+        {
             /*if (!File.Exists("Sharkbite.Thresher.dll"))
             {
                 Server.UseGlobalChat = false;
                 Server.s.Log("[GlobalChat] The IRC dll was not found!");
                 return;
             }*/
-            server = "irc.geekshed.net"; channel = "#MCForge"; this.nick = nick.Replace(" ", "");
+            server = "irc.geekshed.net"; 
+            channel = "#MCForge";
+            this.nick = nick.Replace(" ", "");
             connection = new Connection(new ConnectionArgs(nick, server), false, false);
-            if (Server.UseGlobalChat) {
+
+            if (Server.UseGlobalChat)
+            {
                 // Regster events for incoming
                 connection.Listener.OnNickError += new NickErrorEventHandler(Listener_OnNickError);
                 connection.Listener.OnRegistered += new RegisteredEventHandler(Listener_OnRegistered);
@@ -62,43 +69,54 @@ namespace MCForge {
                 connection.Listener.OnDisconnected += new DisconnectedEventHandler(Listener_OnDisconnected);
             }
         }
-        public void Say(string message, Player p = null) {
-            if (p != null && p.muted) {
+        public void Say(string message, Player p = null)
+        {
+            if (p != null && p.muted)
+            {
                 Player.SendMessage(p, "*Tears* You aren't allowed to talk to the nice people of global chat");
             }
-            if ((p == null && !Server.canusegc) || (p != null && !p.canusegc)) {
-                Player.SendMessage(p, "You can no longer use the GC!"); 
+            if ((p == null && !Server.canusegc) || (p != null && !p.canusegc))
+            {
+                Player.SendMessage(p, "You can no longer use the GC!");
                 return;
             }
             #region General rules
-            if (message.Contains("minecraft.net/classic/play/")) {
-                Player.SendMessage(p, "No server links Mr whale!"); 
-                if (p == null) {
+            if (message.Contains("minecraft.net/classic/play/"))
+            {
+                Player.SendMessage(p, "No server links Mr whale!");
+                if (p == null)
+                {
                     Server.gcmultiwarns++;
                 }
-                else {
-                    p.multi++; 
-                    Command.all.Find("gcrules").Use(p, ""); 
-                } 
-                return; 
-            }
-            if (message.Contains("http://") || message.Contains("https://") || message.Contains("www.")) {
-                Player.SendMessage(p, "No links!");
-                if (p == null) {
-                    Server.gcmultiwarns++;
-                }
-                else {
+                else
+                {
                     p.multi++;
-                    Command.all.Find("gcrules").Use(p, ""); 
+                    Command.all.Find("gcrules").Use(p, "");
                 }
-                return; 
+                return;
             }
-            if (Player.HasBadColorCodes(message)) {
+            if (message.Contains("http://") || message.Contains("https://") || message.Contains("www."))
+            {
+                Player.SendMessage(p, "No links!");
+                if (p == null)
+                {
+                    Server.gcmultiwarns++;
+                }
+                else
+                {
+                    p.multi++;
+                    Command.all.Find("gcrules").Use(p, "");
+                }
+                return;
+            }
+            if (Player.HasBadColorCodes(message))
+            {
                 Player.SendMessage(p, "Can't let you do that Mr whale!");
                 if (p != null) { p.Kick("Kicked for trying to crash all players!"); }
-                return; 
+                return;
             }
-            if (message.ToLower().Contains(Server.name.ToLower())) {
+            if (message.ToLower().Contains(Server.name.ToLower()))
+            {
                 Player.SendMessage(p, "Let's not advertise Mr whale!");
                 if (p != null) { p.multi++; }
                 else { Server.gcmultiwarns++; }
@@ -106,20 +124,23 @@ namespace MCForge {
             }
             #endregion
             #region Repeating message spam
-            if ((p == null ? Server.gclastmsg : p.lastmsg) == message.ToLower()) {
+            if ((p == null ? Server.gclastmsg : p.lastmsg) == message.ToLower())
+            {
                 if (p == null) { Server.gcspamcount++; Server.gcmultiwarns++; }
                 else { p.spamcount++; p.multi++; }
                 Player.SendMessage(p, "Don't send repetitive messages!");
-                if ((p == null ? Server.gcspamcount : p.spamcount) >= 4) {
+                if ((p == null ? Server.gcspamcount : p.spamcount) >= 4)
+                {
                     if (p == null) { Server.canusegc = false; }
                     else { p.canusegc = false; }
-                    Player.SendMessage(p, "You can no longer use the gc! Reason: repetitive message spam"); 
-                    return; 
+                    Player.SendMessage(p, "You can no longer use the gc! Reason: repetitive message spam");
+                    return;
                 }
                 if ((p == null ? Server.gcspamcount : p.spamcount) >= 2) { return; }
                 return;
             }
-            else {
+            else
+            {
                 if (p != null) { p.lastmsg = message.ToLower(); p.spamcount = 0; }
                 else { Server.gclastmsg = message.ToLower(); Server.gcspamcount = 0; }
             }
@@ -142,72 +163,125 @@ namespace MCForge {
                 if (rage >= 10) { return; }
             }
             #endregion*/
+
             #region Flooding
+
             TimeSpan t = DateTime.Now - (p == null ? Server.gclastmsgtime : p.lastmsgtime);
-            if (t < new TimeSpan(0, 0, 1)) {
+
+            if (t < new TimeSpan(0, 0, 1))
+            {
                 Player.SendMessage(p, "Stop the flooding buddy!");
-                if (p == null) { Server.gcfloodcount++; Server.gcmultiwarns++; }
-                else { p.floodcount++; p.multi++; }
-                if ((p == null ? Server.gcfloodcount : p.floodcount) >= 5) {
-                    if (p == null) { Server.canusegc = false; }
-                    else { p.canusegc = false; }
-                    Player.SendMessage(p, "You can no longer use the gc! Reason: flooding"); 
+
+                if (p == null)
+                {
+                    Server.gcfloodcount++;
+                    Server.gcmultiwarns++;
+                }
+                else
+                {
+                    p.floodcount++;
+                    p.multi++;
+                }
+
+                if ((p == null ? Server.gcfloodcount : p.floodcount) >= 5)
+                {
+                    if (p == null)
+                        Server.canusegc = false;
+                    else 
+                        p.canusegc = false;
+
+                    Player.SendMessage(p, "You can no longer use the gc! Reason: flooding");
                 }
                 if ((p == null ? Server.gcfloodcount : p.floodcount) >= 3) { return; }
             }
-            if (p != null) { p.lastmsgtime = DateTime.Now; }
-            else { Server.gclastmsgtime = DateTime.Now; }
+            if (p != null)
+            {
+                p.lastmsgtime = DateTime.Now;
+            }
+            else
+            {
+                Server.gclastmsgtime = DateTime.Now;
+            }
+
             #endregion
 
-            if ((p == null ? Server.gcmultiwarns : p.multi) >= 10) {
-                if (p == null) { Server.canusegc = false; }
-                else { p.canusegc = false; }
-                Player.SendMessage(p, "You can no longer use the gc! Reason: multiple offenses!"); 
-                return; 
+            if ((p == null ? Server.gcmultiwarns : p.multi) >= 10)
+            {
+                if (p == null) 
+                    Server.canusegc = false;
+                else 
+                    p.canusegc = false;
+                Player.SendMessage(p, "You can no longer use the gc! Reason: multiple offenses!");
+
+                return;
             }
+
+            if (String.IsNullOrEmpty(message.Replace("Console:", "").Trim()))
+            {
+                Player.SendMessage(p, "You should send some text!");
+                return;
+            }
+
             if (OnNewSayGlobalMessage != null)
                 OnNewSayGlobalMessage(p == null ? "Console" : p.name, message);
+
             if (Server.UseGlobalChat && IsConnected())
                 connection.Sender.PublicMessage(channel, message);
         }
-        public void Pm(string user, string message) {
+
+
+        public void Pm(string user, string message)
+        {
             if (Server.UseGlobalChat && IsConnected())
                 connection.Sender.PrivateMessage(user, message);
         }
-        public void Reset() {
-            if (!Server.UseGlobalChat) return;
+
+
+        public void Reset()
+        {
+            if (!Server.UseGlobalChat)
+                return;
             reset = true;
             retries = 0;
             Disconnect("Global Chat bot resetting...");
             Connect();
         }
 
-        void Listener_OnJoin(UserInfo user, string channel) {
+        void Listener_OnJoin(UserInfo user, string channel)
+        {
             if (user.Nick == nick)
                 Server.s.Log("Joined the Global Chat!");
         }
 
-        void Listener_OnError(ReplyCode code, string message) {
+        void Listener_OnError(ReplyCode code, string message)
+        {
             //Server.s.Log("IRC Error: " + message);
         }
 
-        void Listener_OnPublic(UserInfo user, string channel, string message) {
+        void Listener_OnPublic(UserInfo user, string channel, string message)
+        {
             //string allowedchars = "1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./!@#$%^*()_+QWERTYUIOPASDFGHJKL:\"ZXCVBNM<>? ";
             //string msg = message;
-            if (message.Contains("^UGCS")) {
+            if (message.Contains("^UGCS"))
+            {
                 Server.UpdateGlobalSettings();
                 return;
             }
-            if (message.Contains("^IPGET ")) {
-                foreach (Player p in Player.players) {
-                    if (p.name == message.Split(' ')[1]) {
-                        if (Server.UseGlobalChat && IsConnected()) {
+            if (message.Contains("^IPGET "))
+            {
+                foreach (Player p in Player.players)
+                {
+                    if (p.name == message.Split(' ')[1])
+                    {
+                        if (Server.UseGlobalChat && IsConnected())
+                        {
                             connection.Sender.PublicMessage(channel, "^IP " + p.name + ": " + p.ip);
                         }
                     }
                 }
             }
-            if (message.Contains("^SENDRULES ")) { //^GETPLAYERINFO NICK PLAYER
+            if (message.Contains("^SENDRULES "))
+            { //^GETPLAYERINFO NICK PLAYER
                 if (Server.devs.Contains(user.Nick.ToLower())) { Player.GlobalMessage("JUSTATEST"); }
                 else { Player.GlobalMessage("NOTATEST"); }
                 string[] split = message.Split(' ');
@@ -215,11 +289,14 @@ namespace MCForge {
                 if (Server.GlobalChatNick != split[1]) { return; }
                 Player p = Player.Find(split[2]);
                 if (p == null) { return; }
-                Command.all.Find("gcrules").Use(p, "");    
+                Command.all.Find("gcrules").Use(p, "");
             }
-            if (message.Contains("^GETINFO ")) {
-                if (Server.GlobalChatNick == message.Split(' ')[1]) {
-                    if (Server.UseGlobalChat && IsConnected()) {
+            if (message.Contains("^GETINFO "))
+            {
+                if (Server.GlobalChatNick == message.Split(' ')[1])
+                {
+                    if (Server.UseGlobalChat && IsConnected())
+                    {
                         connection.Sender.PublicMessage(channel, "^NAME: " + Server.name);
                         connection.Sender.PublicMessage(channel, "^MOTD: " + Server.motd);
                         connection.Sender.PublicMessage(channel, "^VERSION: " + Server.version);
@@ -229,14 +306,23 @@ namespace MCForge {
                     }
                 }
             }
-            if (message.StartsWith("^")) { return; }
+            if (message.StartsWith("^")) 
+                return;
+
             message = message.MCCharFilter();
+
+            if (String.IsNullOrEmpty(message))
+                return;
+
             if (Player.MessageHasBadColorCodes(null, message))
                 return;
-            if (OnNewRecieveGlobalMessage != null) {
+
+            if (OnNewRecieveGlobalMessage != null)
                 OnNewRecieveGlobalMessage(user.Nick, message);
-            }
-            if (Server.devs.Contains(message.Split(':')[0]) && !message.StartsWith("[Dev]") && !message.StartsWith("[Developer]")) { message = "[Dev]" + message; }
+            
+            if (Server.devs.Contains(message.Split(':')[0]) && !message.StartsWith("[Dev]") && !message.StartsWith("[Developer]")) 
+                message = "[Dev]" + message;
+
             /*try { 
                 if(GUI.GuiEvent != null)
                 GUI.GuiEvents.GlobalChatEvent(this, "> " + user.Nick + ": " + message); }
@@ -244,42 +330,53 @@ namespace MCForge {
             Player.GlobalMessage(String.Format("{0}>[Global] {1}: &f{2}", Server.GlobalChatColor, user.Nick, Server.profanityFilter ? ProfanityFilter.Parse(message) : message), true);
         }
 
-        void Listener_OnRegistered() {
+        void Listener_OnRegistered()
+        {
             reset = false;
             retries = 0;
             connection.Sender.Join(channel);
         }
 
-        void Listener_OnDisconnected() {
+        void Listener_OnDisconnected()
+        {
             if (!reset && retries < 5) { retries++; Connect(); }
         }
 
-        void Listener_OnNickError(string badNick, string reason) {
+        void Listener_OnNickError(string badNick, string reason)
+        {
             Server.s.Log("Global Chat nick \"" + badNick + "\" is  taken, please choose a different nick.");
         }
 
-        void Listener_OnKick(UserInfo user, string channel, string kickee, string reason) {
-            if (kickee.Trim().ToLower() == nick.ToLower()) {
+        void Listener_OnKick(UserInfo user, string channel, string kickee, string reason)
+        {
+            if (kickee.Trim().ToLower() == nick.ToLower())
+            {
                 Server.s.Log("Kicked from Global Chat: " + reason);
-                if (OnGlobalKicked != null) OnGlobalKicked(reason);
+                if (OnGlobalKicked != null)
+                    OnGlobalKicked(reason);
                 Server.s.Log("Attempting to rejoin...");
                 connection.Sender.Join(channel);
             }
 
         }
 
-        public void Connect() {
-            if (!Server.UseGlobalChat || Server.shuttingDown) return;
+        public void Connect()
+        {
+            if (!Server.UseGlobalChat || Server.shuttingDown)
+                return;
             try { connection.Connect(); }
             catch { }
         }
 
-        public void Disconnect(string reason) {
+        public void Disconnect(string reason)
+        {
             if (Server.UseGlobalChat && IsConnected()) { connection.Disconnect(reason); Server.s.Log("Disconnected from Global Chat!"); }
         }
 
-        public bool IsConnected() {
-            if (!Server.UseGlobalChat) return false;
+        public bool IsConnected()
+        {
+            if (!Server.UseGlobalChat)
+                return false;
             try { return connection.Connected; }
             catch { return false; }
         }
